e4b1b635c05307d583214fa17a947fa998684597
Juan Olvera <juan.olvera@endurance.com>
2017-03-29 19:37:17 -0500

    add elixir-livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,30 @@
 var elixir = require('laravel-elixir');
+require('laravel-elixir-livereload');
 
 elixir(function(mix) {
-  mix.sass('./scss/style.scss', './style.css')
-    .scripts([
-      './js/customizer.js',
-      './js/navigation.js',
-      './js/skip-link-focus-fix.js'],
-      './scripts.js')
-    .version(['./style.css', './scripts.js'])
-    .browserSync({
-      notify: false,
-      port: 9001,
-      proxy: {
-        target: 'whiteboard.dev'
-      }
-    });
+  mix.sass('./scss/style.scss', './style.css');
+
+  mix.scripts([
+    './js/customizer.js',
+    './js/navigation.js',
+    './js/skip-link-focus-fix.js'
+  ],
+    './scripts.js'
+  );
+
+  mix.version(['./style.css', './scripts.js']);
+
+  mix.browserSync({
+    notify: false,
+    port: 9001,
+    proxy: {
+      target: 'whiteboard.dev'
+    }
+  });
+
+  mix.livereload([
+    './**/*.php',
+    './*.css',
+    './*.js'
+  ]);
 });
