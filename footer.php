8eb5ae41bf750518eb47a0345d57f51aa570947e
Juan Olvera <jolvera@Juans-MacBook-Pro-2.local>
2017-06-16 02:28:16 -0500

    small fixes

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@
         <div class="site-info">
           <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'whiteboard' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'whiteboard' ), 'WordPress' ); ?></a>
           <span class="sep"> | </span>
-          <?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'whiteboard' ), 'whiteboard', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+          <?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'whiteboard' ), 'Whiteboard', '<a href="https://adwhite.com" rel="designer">adWhite</a>' ); ?>
         </div><!-- .site-info -->
       </div> <!-- .row -->
     </div> <!-- .container -->
