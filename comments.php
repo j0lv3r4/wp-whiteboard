a6e6950b1b67271e63e3a956517e0ac91c768a07
yoob5 <yoob5@posteo.de>
2018-07-11 17:58:46 -0500

    fix layout

--- comments.php
+++ comments.php
@@ -26,7 +26,7 @@ if ( post_password_required() ) {
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) :
 		?>
-		<h2 class="comments-title">
+		<h4 class="comments-title">
 			<?php
 			$low_dash_comment_count = get_comments_number();
 			if ( '1' === $low_dash_comment_count ) {
--- search.php
+++ search.php
@@ -10,50 +10,51 @@
 get_header();
 ?>
 
-	<section id="primary" class="content-area">
-		<div class="contianer">
-			<div class="row">
-				<main id="main" class="site-main">
+	<section id="primary" class="container content-area">
+		<div class="row">
+			<main id="main" class="col-md-8 site-main">
 
-				<?php if ( have_posts() ) : ?>
+			<?php if ( have_posts() ) : ?>
 
-					<header class="page-header">
-						<h1 class="page-title">
-							<?php
-							/* translators: %s: search query. */
-							printf( esc_html__( 'Search Results for: %s', 'low-dash' ), '<span>' . get_search_query() . '</span>' );
-							?>
-						</h1>
-					</header><!-- .page-header -->
+				<header class="page-header">
+					<h1 class="page-title">
+						<?php
+						/* translators: %s: search query. */
+						printf( esc_html__( 'Search Results for: %s', 'low-dash' ), '<span>' . get_search_query() . '</span>' );
+						?>
+					</h1>
+				</header><!-- .page-header -->
 
-					<?php
-					/* Start the Loop */
-					while ( have_posts() ) :
-						the_post();
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) :
+					the_post();
 
-						/**
-						 * Run the loop for the search to output the results.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-search.php and that will be used instead.
-						 */
-						get_template_part( 'template-parts/content', 'search' );
+					/**
+					 * Run the loop for the search to output the results.
+					 * If you want to overload this in a child theme then include a file
+					 * called content-search.php and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', 'search' );
 
-					endwhile;
+				endwhile;
 
-					the_posts_navigation();
+				the_posts_navigation();
 
-				else :
+			else :
 
-					get_template_part( 'template-parts/content', 'none' );
+				get_template_part( 'template-parts/content', 'none' );
 
-				endif;
-				?>
+			endif;
+			?>
 
-				</main><!-- #main -->
-			</div> <!-- .row -->
-		</div> <!-- .container -->
+			</main><!-- #main -->
+
+			<?php
+			get_sidebar();
+			?>
+		</div> <!-- .row -->
 	</section><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
